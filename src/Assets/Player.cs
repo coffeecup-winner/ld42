6d78767cb78be1d1d1fdd17556cf364afd65afa6
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2018-08-07 19:33:33 -0700

    Add player

---
+++ src/Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private static readonly float Speed = 3.0f;
+    private static readonly float RotationSpeed = 170.0f;
+
+    void Start() {
+        // var fileData = File.ReadAllBytes("Assets/Textures/bullet.png");
+        // var texture = new Texture2D(2, 2);
+        // texture.LoadImage(fileData);
+        // var sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(1.0f, 1.0f));
+
+        // var bullet = new GameObject();
+        // var renderer = bullet.AddComponent<SpriteRenderer>();
+        // renderer.sprite = sprite;
+
+        // bullet.transform.parent = transform;
+    }
+
+    void Update() {
+        float translation = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+        float rotation = Input.GetAxis("Horizontal") * RotationSpeed * Time.deltaTime;
+
+        transform.Translate(0, translation, 0);
+        transform.Rotate(0, 0, -rotation);
+    }
+}
