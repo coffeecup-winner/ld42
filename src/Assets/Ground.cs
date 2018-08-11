65ac4ac3ce3a5eb8057b643bb07f4943ea7d29ac
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2018-08-11 00:10:47 -0700

    Add colliders to ground

---
+++ src/Assets/Ground.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Ground : MonoBehaviour {
+    private Color normalColor;
+    private Color mouseOverColor;
+
+    void Start() {
+        normalColor = GetComponent<SpriteRenderer>().color;
+        mouseOverColor = new Color(normalColor.r * 1.1f, normalColor.g * 1.1f, normalColor.b * 1.1f);
+    }
+
+    void Update() {
+    }
+
+    void OnMouseEnter() {
+        GetComponent<SpriteRenderer>().color = mouseOverColor;
+    }
+
+    void OnMouseExit() {
+        GetComponent<SpriteRenderer>().color = normalColor;
+    }
+}
